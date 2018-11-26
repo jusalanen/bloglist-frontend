a628886ad7ae677a63f41e2977497040d4215246
Jussi Alanen <jussi.alanen@fonet.fi>
2018-11-26 17:03:59 +0200

    lisää loginService, Notification ja css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/loginService'
+import Notification from './components/Notification'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      blogs: []
+      blogs: [],
+      username: '',
+      password: '',
+      user: null,
+      error: null
     }
   }
 
@@ -14,9 +20,68 @@ class App extends React.Component {
     blogService.getAll().then(blogs =>
       this.setState({ blogs })
     )
-  } 
+  }
+
+  login = async (event) => {
+    event.preventDefault()
+    try{
+      const user = await loginService.login({
+        username: this.state.username,
+        password: this.state.password
+      })
+  
+      this.setState({ username: '', password: '', user})
+    } catch(exception) {
+      this.setState({
+        error: 'käyttäjätunnus tai salasana virheellinen',
+      })
+      setTimeout(() => {
+        this.setState({ error: null })
+      }, 5000)
+    }
+  }
+
+  handleLoginFieldChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
 
   render() {
+    if (this.state.user === null) {
+      return (
+        <div>
+        <h1>Muistiinpanot</h1>
+
+        <Notification message={this.state.error} />
+
+        <div>
+          <h2>Kirjaudu sovellukseen</h2>
+          
+          <form onSubmit={this.login}>
+          <div>
+            käyttäjätunnus  
+              <input
+                type="text"
+                name="username"
+                value={this.state.username}
+                onChange={this.handleLoginFieldChange}
+              />
+          </div>
+          <div>
+            salasana  
+              <input
+                type="password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleLoginFieldChange}
+              />
+          </div>
+          <button type="submit">kirjaudu</button>
+          </form>
+        
+        </div>
+        </div>
+      )
+    }
     return (
       <div>
         <h2>blogs</h2>
