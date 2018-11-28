a6801e435e7b4cf401018e4d78af7b27dcde0eef
Jussi Alanen <jussi.alanen@fonet.fi>
2018-11-28 17:42:06 +0200

    toimiva local storage

--- src/App.js
+++ src/App.js
@@ -12,7 +12,10 @@ class App extends React.Component {
       username: '',
       password: '',
       user: null,
-      error: null
+      error: null,
+      title: '',
+      author: '',
+      url: ''
     }
   }
 
@@ -20,6 +23,13 @@ class App extends React.Component {
     blogService.getAll().then(blogs =>
       this.setState({ blogs })
     )
+
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      this.setState({user})
+      blogService.setToken(user.token)
+    }
   }
 
   login = async (event) => {
@@ -31,7 +41,8 @@ class App extends React.Component {
       })
 
       window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
-  
+      blogService.setToken(user.token)
+
       this.setState({ username: '', password: '', user})
     } catch(exception) {
       this.setState({
@@ -43,7 +54,32 @@ class App extends React.Component {
     }
   }
 
+  addBlog = (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: this.state.title,
+      author: this.state.author,
+      url: this.state.url,
+      user: this.state.user
+    }
+
+    blogService
+      .create(blogObject)
+      .then(newBlog => {
+        this.setState({
+          blogs: this.state.blogs.concat(newBlog),
+          title: '',
+          author: '',
+          url: ''
+        })
+      })
+  }
+
   handleLoginFieldChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  handleBlogFieldChange = (event) => {
     this.setState({ [event.target.name]: event.target.value })
   }
 
@@ -87,10 +123,35 @@ class App extends React.Component {
     return (
       <div>
         <h2>Blogs</h2>
-        <tr><td width='200'>{this.state.user.name} logged in</td>
-        <td width='50'><button onClick = {function() {
+        <p>{this.state.user.name} logged in  </p><button onClick = {function() {
           window.localStorage.removeItem('loggedBlogappUser')
-        }}  >logout</button></td></tr><br></br>
+        }}  >logout</button><br></br>
+        <h4>add new</h4>
+
+        <form onSubmit={this.addBlog}>
+        title_
+        <input
+          type="text"
+          name="title"
+          value={this.state.title}
+          onChange={this.handleBlogFieldChange}
+        /><br></br>
+        author_
+        <input
+          type="text"
+          name="author"
+          value={this.state.author}
+          onChange={this.handleBlogFieldChange}
+        /><br></br>       
+        url_
+       <input
+          type="text"
+          name="url"
+          value={this.state.url}
+          onChange={this.handleBlogFieldChange}
+        /><br></br><br></br>
+          <button type="submit">add</button>
+        </form><br></br>
         {this.state.blogs.map(blog => 
           <Blog key={blog._id} blog={blog}/>
         )}
